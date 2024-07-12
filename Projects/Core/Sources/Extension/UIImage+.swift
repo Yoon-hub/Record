92c46b60924b7ce5397855b6ee86ad6614644a23
yoon <kong4170@naver.com>
2024-07-12 13:37:18 +0900

    feat: UIImage extension resize method 추가

---
+++ Projects/Core/Sources/Extension/UIImage+.swift
@@ -0,0 +1,33 @@
+//
+//  UIImage+.swift
+//  Core
+//
+//  Created by 윤제 on 7/12/24.
+//
+
+import UIKit
+
+public extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage {
+           let size = self.size
+
+           let widthRatio  = targetSize.width  / size.width
+           let heightRatio = targetSize.height / size.height
+
+           // Determine the scale factor that preserves aspect ratio
+           let scaleFactor = min(widthRatio, heightRatio)
+
+           // Compute the new image size that preserves aspect ratio
+           let scaledImageSize = CGSize(width: size.width * scaleFactor, height: size.height * scaleFactor)
+
+           // Create a new image context
+           UIGraphicsBeginImageContextWithOptions(scaledImageSize, false, 0.0)
+           self.draw(in: CGRect(origin: .zero, size: scaledImageSize))
+
+           // Create a new image from the context
+           let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+           UIGraphicsEndImageContext()
+
+           return resizedImage!
+       }
+}
