45278420c66bc2427833b8a616db8993830b6ac4
yoon <kong4170@naver.com>
2024-07-17 16:27:23 +0900

    feat: MainNavigator 추가

---
+++ Projects/App/Sources/Application/Navigator/MainNaviagator.swift
@@ -0,0 +1,27 @@
+//
+//  MainNaviagator.swift
+//  App
+//
+//  Created by 윤제 on 7/16/24.
+//
+
+import UIKit
+
+import Core
+
+protocol MainNaviagatorProtocol: BaseNavigator {
+    func toMovieAdd()
+}
+
+final class MainNaviagator: MainNaviagatorProtocol {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func toMovieAdd() {
+        
+    }
+}
