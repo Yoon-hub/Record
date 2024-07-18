eba46ceb639c229425166a5de14f09f0343a7759
yoon <kong4170@naver.com>
2024-07-18 10:41:07 +0900

    feat: Then 기능

--- Projects/Core/Sources/Utilities/Then.swift
+++ Projects/Core/Sources/Utilities/Then.swift
@@ -5,4 +5,19 @@
 //  Created by 윤제 on 7/18/24.
 //
 
-import Foundation
+import UIKit
+
+public protocol Then {}
+
+extension Then where Self: AnyObject {
+    
+    @inlinable
+    public func then(
+        _ block: (Self) throws -> Void
+    ) rethrows -> Self {
+      try block(self)
+      return self
+    }
+}
+
+extension UIView: Then {}
