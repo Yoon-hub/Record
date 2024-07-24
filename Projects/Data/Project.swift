43d08d6e6882725ca135ff990193465dc0e0bce7
yoon <kong4170@naver.com>
2024-07-24 15:43:38 +0900

    chore: Domain, Data 모듈 추가

---
+++ Projects/Data/Project.swift
@@ -0,0 +1,22 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by 윤제 on 7/23/24.
+//
+
+import ProjectDescription
+
+let dataProject = Project(name: "Data", targets: [
+    .target(
+        name: "Data",
+        destinations: .iOS,
+        product: .staticLibrary,
+        bundleId: "record.Data.com",
+        deploymentTargets: .iOS("17.0"),
+        sources: ["Sources/**"],
+        dependencies: [
+            .project(target: "Domain", path: "../Domain"),
+        ]
+    )
+])
---
+++ Projects/Domain/Project.swift
@@ -0,0 +1,22 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by 윤제 on 7/23/24.
+//
+
+import ProjectDescription
+
+let domainProject = Project(name: "Domain", targets: [
+    .target(
+        name: "Domain",
+        destinations: .iOS,
+        product: .staticLibrary,
+        bundleId: "record.Domain.com",
+        deploymentTargets: .iOS("17.0"),
+        sources: ["Sources/**"],
+        dependencies: [
+            
+        ]
+    )
+])
