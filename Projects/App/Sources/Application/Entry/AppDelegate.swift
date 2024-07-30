ac658a17dc9aa92292d30395eb1d55133036e798
yoon <kong4170@naver.com>
2024-07-30 15:08:53 +0900

    feat: MoveDIContainer 추가

--- Projects/App/Sources/Application/Entry/AppDelegate.swift
+++ Projects/App/Sources/Application/Entry/AppDelegate.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
+import Core
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        
+        let containers: [BaseContainer] = [
+            MovieDIContainer(),
+        ]
+        containers.forEach {
+            $0.registerDependencies()
+        }
+        
         return true
     }
 }
