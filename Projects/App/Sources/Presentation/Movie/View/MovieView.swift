844d99de8ff7302f7ca3a22662ad6e3bf7856bc3
yoon <kong4170@naver.com>
2024-07-31 11:00:20 +0900

    feat: MovieView CollectionView UI 추가

--- Projects/App/Sources/Presentation/Movie/View/MovieView.swift
+++ Projects/App/Sources/Presentation/Movie/View/MovieView.swift
@@ -14,22 +14,43 @@ import PinLayout
 
 final class MovieView: UIView, BaseView {
 
+    let collectionView: UICollectionView = {
+        let spacing: CGFloat = 10
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionViewFlowLayout.scrollDirection = .vertical
+        collectionViewFlowLayout.minimumLineSpacing = 10
+        collectionViewFlowLayout.minimumInteritemSpacing = 10
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        
+        let width = (UIScreen.main.bounds.width / 2) - (3*spacing)
+        
+        collectionViewFlowLayout.itemSize = CGSize(width: width, height: width + 52)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: MainCollectionViewCell.identifier)
+        return collectionView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
-        setUI()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setUI()
+    }
+    
     func configure() {
-
+        [collectionView].forEach {addSubview( $0 ) }
     }
     
     func setUI() {
-
+        collectionView.pin
+            .all()
     }
 }
