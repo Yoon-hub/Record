aa9feddcd5a109c5ea9bf4e4b43efde7edf380f6
yoon <kong4170@naver.com>
2024-08-14 09:09:16 +0900

    feat: fscalendar custom Cell 추가

--- Projects/App/Sources/Presentation/Calendar/View/Cell/FScalendarCustomCell.swift
+++ Projects/App/Sources/Presentation/Calendar/View/Cell/FScalendarCustomCell.swift
@@ -5,4 +5,51 @@
 //  Created by 윤제 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+import Core
+import Design
+
+import FSCalendar
+import PinLayout
+
+final class FScalendarCustomCell: FSCalendarCell, BaseView {
+    
+    let line = UIView().then {
+        $0.backgroundColor = .systemGray6
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // PinLayout 적용
+        setUI()
+    }
+    
+    func configure() {
+        [line].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    func setUI() {
+        titleLabel.pin
+            .top(2)
+            .hCenter()
+            .sizeToFit()
+        
+        line.pin
+            .top(-4)
+            .height(1)
+            .horizontally()
+    }
+}
