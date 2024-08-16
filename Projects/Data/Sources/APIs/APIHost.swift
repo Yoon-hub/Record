aee2946447204e0371580b04046f671b0a9e7216
yoon <kong4170@naver.com>
2024-08-16 10:31:33 +0900

    feat: 공공 API base URL

---
+++ Projects/Data/Sources/APIs/APIHost.swift
@@ -0,0 +1,12 @@
+//
+//  APIHost.swift
+//  Data
+//
+//  Created by 윤제 on 8/14/24.
+//
+
+import Foundation
+
+public enum APIHost {
+    static let baseURL = "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService"
+}
