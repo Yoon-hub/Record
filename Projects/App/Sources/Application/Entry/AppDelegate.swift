f3bc293311a0796ec9c57a1934156c0aa9e8f71d
yoon <kong4170@naver.com>
2024-08-19 13:16:20 +0900

    feat: 최초 진입 시 공휴일 정보 받아오는 로직 수정

--- Projects/App/Sources/Application/Entry/AppDelegate.swift
+++ Projects/App/Sources/Application/Entry/AppDelegate.swift
@@ -16,7 +16,7 @@ import RxSwift
 import RxCocoa
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let disposebag = DisposeBag()
     
@@ -56,11 +56,12 @@ extension AppDelegate {
             for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"] {
                 fetchRestDayUsecase.excute(year: year, month: month)
                     .compactMap {$0}
-                    .bind { $0.forEach { saveRestDayUsecase.execute(restDay: $0) } }
+                    .bind {
+                        $0.forEach { saveRestDayUsecase.execute(restDay: $0) }
+                        isFetchRestDay = "Y"
+                    }
                     .disposed(by: disposebag)
             }
         }
-        
-        isFetchRestDay = "Y"
     }
 }
