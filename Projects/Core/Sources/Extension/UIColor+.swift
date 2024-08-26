2f0645925ff4d2046488034c936fcaf141b81154
yoon <kong4170@naver.com>
2024-08-26 13:21:44 +0900

    feat: UIColor extension hexString 변환 추가

--- Projects/Core/Sources/Extension/UIColor+.swift
+++ Projects/Core/Sources/Extension/UIColor+.swift
@@ -13,4 +13,26 @@ extension UIColor {
     public static var recordColor: UIColor {
         DesignAsset.record.color
     }
+    
+    public var hexString: String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        guard self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+            return "#000000"
+        }
+
+        let r = Int(red * 255.0)
+        let g = Int(green * 255.0)
+        let b = Int(blue * 255.0)
+        let a = Int(alpha * 255.0)
+
+        if a < 255 {
+            return String(format: "#%02X%02X%02X%02X", r, g, b, a)
+        } else {
+            return String(format: "#%02X%02X%02X", r, g, b)
+        }
+    }
 }
