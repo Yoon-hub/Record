9dd235290108ccda1d426c84ee437bc1aef02930
yoon <kong4170@naver.com>
2024-10-23 08:28:22 +0900

    fix: Navigator propertyWrapper 중복코드 수정

--- Projects/Core/Sources/DI/Navigator.swift
+++ Projects/Core/Sources/DI/Navigator.swift
@@ -7,23 +7,4 @@
 
 import Foundation
 
-@propertyWrapper
-public struct Navigator<V> {
-    
-    public let identifier: InjectIdentifier<V>
-    
-    public init() {
-        self.identifier = .by(type: V.self)
-    }
-    
-    public lazy var wrappedValue: V = {
-        let contianer = Container.standard
-        
-        do {
-            let value = try contianer.resolve(identifier)
-            return value
-        } catch {
-            fatalError(error.localizedDescription)
-        }
-    }()
-}
+public typealias Navigator = Injected
